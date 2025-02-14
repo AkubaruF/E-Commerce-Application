49217d257ccb7018a8316802e09bf7be46dd9b29
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-02-14 22:29:50 +0700

    chore: omit products field when empty

--- ECommerceApplication/src/main/java/com/app/payloads/CartDTO.java
+++ ECommerceApplication/src/main/java/com/app/payloads/CartDTO.java
@@ -3,6 +3,8 @@ package com.app.payloads;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartDTO {
-	
+
 	private Long cartId;
 	private Double totalPrice = 0.0;
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
 	private List<ProductDTO> products = new ArrayList<>();
 }
