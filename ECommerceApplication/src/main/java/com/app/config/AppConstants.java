4934aeedbb2dc7c27a8e8d3b38cf760e8fd021dc
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-02-14 21:37:22 +0700

    feat: add coupon constant for sorting

--- ECommerceApplication/src/main/java/com/app/config/AppConstants.java
+++ ECommerceApplication/src/main/java/com/app/config/AppConstants.java
@@ -1,12 +1,13 @@
 package com.app.config;
 
 public class AppConstants {
-	
+
 	public static final String PAGE_NUMBER = "0";
 	public static final String PAGE_SIZE = "2";
 	public static final String SORT_BRANDS_BY = "brandId";
 	public static final String SORT_COUPON_BY = "couponId";
 	public static final String SORT_CATEGORIES_BY = "categoryId";
+	public static final String SORT_COUPON_BY_TITLE = "title";
 	public static final String SORT_PRODUCTS_BY = "productId";
 	public static final String SORT_USERS_BY = "userId";
 	public static final String SORT_ORDERS_BY = "totalAmount";
@@ -17,5 +18,5 @@ public class AppConstants {
 	public static final String[] PUBLIC_URLS = { "/v3/api-docs/**", "/swagger-ui/**", "/api/register/**", "/api/login" };
 	public static final String[] USER_URLS = { "/api/public/**" };
 	public static final String[] ADMIN_URLS = { "/api/admin/**" };
-	
+
 }
