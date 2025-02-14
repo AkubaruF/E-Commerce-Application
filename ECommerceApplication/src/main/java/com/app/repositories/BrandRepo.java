d6b41c60f1ba1d1ea3d207485bded45c71706b49
AkubaruF <112474451+AkubaruF@users.noreply.github.com>
2025-02-14 14:51:06 +0700

    Brand controller with filter

---
+++ ECommerceApplication/src/main/java/com/app/repositories/BrandRepo.java
@@ -0,0 +1,13 @@
+package com.app.repositories;
+
+import com.app.entites.Brand;
+import com.app.entites.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepo extends JpaRepository<Brand, Long> {
+
+	Brand findByBrandName(String brandName);
+
+}
